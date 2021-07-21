dd6db12061f383e99be7c22ed633c9747969fb13
i535676 <yakir.lebovits@sap.com>
2021-07-21 05:12:28 -0400

    added swagger

--- Host2Host/settings.py
+++ Host2Host/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Host2Host/urls.py
+++ Host2Host/urls.py
@@ -13,22 +13,44 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 from Host2Host import settings
 from main import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Host2Host API",
+        default_version='v1',
+        # description="Test description",
+        # terms_of_service="https://www.google.com/policies/terms/",
+        # contact=openapi.Contact(email="contact@snippets.local"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/register/', views.register),
+                  path('admin/', admin.site.urls),
+                  path('api/register/', views.register),
 
-    path('api/venue/create/', views.make_post),
-    path('api/venue/upload_image/', views.upload_image),
-    path('api/venue/book/', views.make_booking),
-    path('api/venue/images/<user>/', views.retrieve_images),
+                  path('api/venue/create/', views.make_post),
+                  path('api/venue/upload_image/', views.upload_image),
+                  path('api/venue/book/', views.make_booking),
+                  path('api/venue/images/<user>/', views.retrieve_images),
 
-    path('api/bookings/', views.retrieve_bookings),
-    path('api/bookings/search/', views.search)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('api/bookings/', views.retrieve_bookings),
+                  path('api/bookings/search/', views.search),
+
+                  url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+                      name='schema-json'),
+                  url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
