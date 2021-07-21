4d4d802e1c8190cb327466612d858ea3bccf1fcf
i535676 <yakir.lebovits@sap.com>
2021-07-21 03:51:46 -0400

    added image retrieval
    can now access previously uploaded venue images

--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
+from typing import List, Dict
+
+from django.db.models import Model
 from django.http import *
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
-
+from django.core import serializers as django_serializers
 from .serializers import *
 
 
@@ -36,9 +39,21 @@ def upload_image(request: Request) -> JsonResponse:
 
 
 @api_view(['POST'])
-def make_booking(request):
+def make_booking(request: Request) -> JsonResponse:
     serializer = BookingSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
     return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def retrieve_images(request: Request, *args: List, **kwargs: Dict) -> JsonResponse:
+    # verify host exists as user
+    try:
+        User.objects.filter(pk=kwargs['user']).get()
+    except Model.DoesNotExist:
+        return JsonResponse(data=None, status=status.HTTP_404_NOT_FOUND)
+
+    queryset = VenueImage.objects.filter(owner_id=kwargs['user'])
+    return JsonResponse(list(queryset.values()), status=status.HTTP_200_OK, safe=False)
