2434ea994f083f7f3b4ae5ab730e654ecd971468
i535676 <yakir.lebovits@sap.com>
2021-07-22 17:14:44 -0400

    connected client to backend

--- client/src/app/registration/registration.module.ts
+++ client/src/app/registration/registration.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 
 import { RegistrationPageRoutingModule } from './registration-routing.module';
@@ -13,7 +13,8 @@ import { RegistrationPage } from './registration.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    RegistrationPageRoutingModule
+    RegistrationPageRoutingModule,
+    HttpClientModule
   ],
   declarations: [RegistrationPage]
 })
